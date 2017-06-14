ResizeLayer gradient fix 20170614

python/caffe/test/test_resize_layer.py
@@ -9,14 +9,17 @@ from scipy import misc
 
 import caffe
 
+in_size = 122
+out_size = 190
+
 def python_param_net_file():
     with tempfile.NamedTemporaryFile(mode='w+', delete=False) as f:
         f.write("""name: 'pythonnet'
 	force_backward: true
         input: 'data'
-	input_shape { dim:1 dim: 300 dim: 512 dim: 512}
+	input_shape { dim:1 dim: 3 dim: 122 dim: 122}
         input: 'label'
-	input_shape { dim:1 dim: 300 dim: 64 dim: 64}
+	input_shape { dim:1 dim: 3 dim: 190 dim: 190}
         layer {
 	 type: 'Resize'
 	 name: 'resize'
@@ -26,7 +29,7 @@ def python_param_net_file():
          resize_param {
 	   function_type: BLOB_ALIGN
 	   intepolation_type: BILINEAR
-	   output_size: 128
+	   #output_size: 128
 	 } }""")
         return f.name
 
@@ -36,24 +39,33 @@ def python_param_net_file():
 class TestLayerWithParam(unittest.TestCase):
     def setUp(self):
         net_file = python_param_net_file()
-	caffe.set_mode_cpu()
+	caffe.set_mode_gpu()
         self.net = caffe.Net(net_file, caffe.TRAIN)
         os.remove(net_file)
 
     def test_forward(self):
-	img = np.zeros((300,512,512)).astype(float)
-	for i in range(100):
-		tmp = misc.imread('../examples/images/cat.jpg')
-		img[i*3:(i+1)*3,...] = misc.imresize(tmp, [512,512]).transpose(2,0,1)
+	img = np.zeros((3,in_size,in_size)).astype(float)
+	tmp = misc.imread('../examples/images/cat.jpg')
+	img[0:3,...] = misc.imresize(tmp, [in_size,in_size]).transpose(2,0,1)
 	self.net.blobs['data'].data[...] = img
 	time1 = time.time()
         self.net.forward()
-	print '!!!!!\n!!!!!\n!!!!!\n!!!!!\n!!!!!\n!!!!!\n', time.time()-time1
+	print 'forward !!!!!\n!!!!!\n!!!!!\n!!!!!\n!!!!!\n!!!!!\n', time.time()-time1
 	plt.subplot(1,2,1)
-	plt.imshow(self.net.blobs['data'].data[0,0:3,...].transpose(1,2,0))
+	plt.imshow(self.net.blobs['data'].data[0,0:3,...].transpose(1,2,0).astype(np.uint8))
 	plt.subplot(1,2,2)
-	plt.imshow(self.net.blobs['resize'].data[0,0:3,...].transpose(1,2,0))
+	plt.imshow(self.net.blobs['resize'].data[0,0:3,...].transpose(1,2,0).astype(np.uint8))
 	plt.show()
 
     def test_backward(self):
+	img = np.zeros((3,out_size,out_size)).astype(float)
+	tmp = misc.imread('../examples/images/cat.jpg')
+	img[...] = misc.imresize(tmp, [out_size,out_size]).transpose(2,0,1)
+	self.net.blobs['resize'].diff[...] = img
+        self.net.backward()
+	plt.subplot(1,2,1)
+	plt.imshow(self.net.blobs['data'].diff[0,0:3,...].transpose(1,2,0).astype(np.uint8))
+	plt.subplot(1,2,2)
+	plt.imshow(self.net.blobs['resize'].diff[0,0:3,...].transpose(1,2,0).astype(np.uint8))
+	plt.show()
 	return