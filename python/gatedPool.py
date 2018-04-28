d20544d413614c34ee4487c39f1ea8029653975b
YCJGG <251623266@qq.com>
2018-04-28 09:22:22 +0800

    gated-pool-modified

--- python/gatedPool.py
+++ python/gatedPool.py
@@ -28,7 +28,8 @@ class gatedPoolLayer(caffe.Layer):
 	# init conv weights
 	self.blobs.add_blob(self.kernel_size, self.kernel_size)
 	self.blobs[0].data[:,:] = torch.randn(self.kernel_size, self.kernel_size).numpy()
-	self.weight = nn.Parameter(torch.from_numpy(self.blobs[0].data[:,:]), requires_grad=True)
+	self.weight = nn.Parameter(torch.from_numpy(self.blobs[0].data[:,:]).cuda(), requires_grad=True)
+	#self.weight = self.weight.cuda()
 	return
 
     def reshape(self, bottom, top):
@@ -36,8 +37,10 @@ class gatedPoolLayer(caffe.Layer):
 	batch_size = bottom[0].data.shape[0]
 	channel = bottom[0].data.shape[1]
 	tmp_weight = torch.stack([self.weight.unsqueeze(0) for _ in range(channel)], dim=0)
+	#tmp_weight = tmp_weight.cuda()
 	# get input
-	self.x = Variable(torch.from_numpy(bottom[0].data[:,:,:,:]), requires_grad=True)
+	self.x = Variable(torch.from_numpy(bottom[0].data[:,:,:,:]).cuda(), requires_grad=True)
+	#self.x = self.x.cuda()
 	# get alpha
 	alpha = F.conv2d(self.x, tmp_weight, stride=self.stride, padding=self.pad, groups=channel)
 	alpha = F.sigmoid(alpha)
@@ -54,16 +57,19 @@ class gatedPoolLayer(caffe.Layer):
 
     def backward(self, top, propagate_down, bottom):
 	# backward
-	self.y.backward(gradient=torch.from_numpy(top[0].diff[:,:,:,:]))
+	gradient=torch.from_numpy(top[0].diff[:,:,:,:])
+	gradient = Variable(gradient.cuda(),requires_grad=False)
+	self.y.backward(gradient = gradient)
 	# get diff w.r.t. data and weight
-	bottom[0].diff[:,:,:,:] = self.x.grad.data.numpy()
-	self.blobs[0].diff[:,:] += self.weight.grad.data.numpy()	# accumulate weight's diff
+	#print(self.x.grad.data.numpy())
+	bottom[0].diff[:,:,:,:] = self.x.grad.data.cpu().numpy()
+	self.blobs[0].diff[:,:] += self.weight.grad.data.cpu().numpy()	# accumulate weight's diff
 	self.y.detach_()
 	# zero grad
 	if not self.x.grad is None:
 		self.x.grad.detach_()
-		self.x.grad.zero_()
+		self.x.grad.data.zero_()
 	if not self.weight.grad is None:
 		self.weight.grad.detach_()
-		self.weight.grad.zero_()
+		self.weight.grad.data.zero_()
 	return
